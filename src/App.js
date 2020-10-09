6baefa83f087643e40f7c4829c70863090170e71
Norden-Tenzin <nordentenzin29@gmail.com>
2020-10-09 10:07:07 -0400

    basic files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import _ from 'lodash'
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import {Grid, Image} from 'semantic-ui-react'
+
+const columns = _.times(16, (i) => (
+  <Grid.Column key={i}>
+    <Image src='/images/wireframe/image.png' />
+  </Grid.Column>
+))
+const GridExampleGrid = () => <Grid>{columns}</Grid>
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Grid stretched>
+        {columns}
+      </Grid>
     </div>
   );
 }
