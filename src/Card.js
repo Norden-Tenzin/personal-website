3ac0794d62860f3997abafa6615b90f38726cb8c
Norden-Tenzin <nordentenzin29@gmail.com>
2021-07-19 11:05:45 -0400

    removed product for now

---
+++ src/Card.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import pic from "./assets/image.jpg";
+import "./stylesheet.scss";
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      header: this.props.header,
+      pic: this.props.pic,
+    };
+  }
+
+  render() {
+    // const { data } = this.state;
+    // console.log(this.state.pic)
+    return (
+      <section className="card">
+        <img src={this.state.pic} className="image"/>
+        <p className="header">{this.state.header}</p>
+        {/* <p className="detail">Some Text</p> */}
+      </section>
+    );
+  }
+}
+export default Card;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl6xFSZOXDjY50fgXCp2kLYnCrbavA4Jg",
+  authDomain: "personal-website-3e90d.firebaseapp.com",
+  databaseURL: "https://personal-website-3e90d.firebaseio.com",
+  projectId: "personal-website-3e90d",
+  storageBucket: "personal-website-3e90d.appspot.com",
+  messagingSenderId: "264162876341",
+  appId: "1:264162876341:web:c36037c3f35dec9749d7fc",
+  measurementId: "G-GRPJWZD2PZ",
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
