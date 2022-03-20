b9ddceb778dcfd9db143d0ad021edae950101a36
Tenzin Norden <nordentenzin29@gmail.com>
2022-03-20 13:25:29 -0400

    fixed header to be flex

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -66,118 +66,31 @@ export default class LandingPage extends React.Component {
     return (
       <div>
         <div className="landing" id="home">
-          {isDesktop ? (
-            <div className="header_holder">
-              <Grid container columns={2}>
-                <Grid.Column width={3}>
-                  <Link
-                    activeClass="active"
-                    to="home"
-                    spy={true}
-                    smooth={true}
-                    offset={-360}
-                    duration={500}
-                  >
-                    <h1 className="header_logo">tn.</h1>
-                  </Link>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <div className="header_links_col">
-                    {/* <Link
-                      activeClass="active"
-                      to="home"
-                      spy={true}
-                      smooth={true}
-                      offset={-360}
-                      duration={500}
-                    >
-                      <h1 className="links">Home</h1>
-                    </Link> */}
-                    <Link
-                      activeClass="active"
-                      to="projects"
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={500}
-                    >
-                      <h1 className="links">Projects</h1>
-                    </Link>
-                    <Link
-                      activeClass="active"
-                      to="morse"
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={500}
-                    >
-                      <h1 className="links">Demo</h1>
-                    </Link>
-                    {/* <Link
-                      activeClass="active"
-                      to="about"
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={500}
-                    >
-                      <h1 className="links">About</h1>
-                    </Link> */}
-                  </div>
-                </Grid.Column>
-              </Grid>
+          <div className="header_container">
+            <div className="header_logo">tn.</div>
+            <div className="header_links_container">
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                <h1 className="links">Projects</h1>
+              </Link>
+              <Link
+                activeClass="active"
+                to="morse"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                <h1 className="links">Demo</h1>
+              </Link>
             </div>
-          ) : (
-            <div className="header_holder--responsive">
-              <Grid container>
-                <Grid.Row style={{ padding: 0 }}>
-                  <h1 className="header_logo">tn.</h1>
-                </Grid.Row>
-                <Grid.Row style={{ padding: 0 }}>
-                  <Link
-                    activeClass="active"
-                    to="home"
-                    spy={true}
-                    smooth={true}
-                    offset={-360}
-                    duration={500}
-                  >
-                    <h1 className="header_link">Home</h1>
-                  </Link>
-                  <Link
-                    activeClass="active"
-                    to="projects"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                  >
-                    <h1 className="header_link">Projects</h1>
-                  </Link>
-                  <Link
-                    activeClass="active"
-                    to="morse"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                  >
-                    <h1 className="header_link">Demo</h1>
-                  </Link>
-                  {/* <Link
-                      activeClass="active"
-                      to="about"
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={500}
-                    >
-                      <h1 className="header_link">About</h1>
-                    </Link> */}
-                </Grid.Row>
-              </Grid>
-            </div>
-          )}
+          </div>
 
           <div className="introduction">
             <p className="paragraph">
@@ -205,9 +118,6 @@ export default class LandingPage extends React.Component {
               >
                 <img src={instagramIcon} className="icon" />
               </a>
-              {/* <a href={pdf} className="icon_link">
-                <div className="link">Click Here for Resume</div>
-              </a> */}
             </div>
           </div>
         </div>
@@ -294,9 +204,7 @@ export default class LandingPage extends React.Component {
                   >
                     Personal Website
                   </a>
-                  <p className="info">
-                    used React to build this website.
-                  </p>
+                  <p className="info">used React to build this website.</p>
                 </div>
               </div>
             </div>
